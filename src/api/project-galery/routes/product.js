408b1a36e72c435be482a67d94576ef0ec934e2d
Maximo <maximo2004lfn@gmail.com>
2026-06-22 19:41:03 -0300

    projects collection added

---
+++ src/api/project-galery/routes/product.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * article router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::project-gallery.project-gallery');
